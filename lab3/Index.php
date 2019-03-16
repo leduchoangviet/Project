d74b7fdebaa9f44efe21004ed3f1cf03deb50a92
leduchoangviet <48613535+leduchoangviet@users.noreply.github.com>
2019-03-16 14:44:08 +0700

    Update file index

--- lab3/Index.php
+++ lab3/Index.php
@@ -11,13 +11,5 @@
 
 
 
-<?php include_once("header.php"); ?>
-<ul class="menu">
-	<li>
-		<a href="/lab3/list_product.php"> danh sach san pham</a>
-	</li>
-	<li>
-		<a href="/lab3/add_product.php">them san pham</a>
-	</li>
-</ul>
-<?php include_once("footer.php"); ?>
+
+
