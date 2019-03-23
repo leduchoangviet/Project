9243ca171a9cd2ee5426b9f6a2ddc20913cd8c0f
hoangviet270197 <leduchoangviet1997@gmail.com>
2019-03-23 13:58:50 +0700

    them nhieu file lam

--- lab3/Index.php
+++ lab3/Index.php
@@ -15,10 +15,15 @@
 	</li>
 
 </ul>
+
 <?php include_once("footer.php"); ?>
-<li>
+	<li>
 		<a href="/lab3/add_product.php">them san pham</a>
 	</li>
+	<li>
+		<a href="/lab3/add_product.php">them san pham</a>
+	</li>
+	
 
 
 
